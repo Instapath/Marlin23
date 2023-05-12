4625fc70043b23ec2c85fe0f42406cddf2ec2993
samluethy <sluethy@instapathbio.com>
2023-05-12 16:03:41 -0400

    fixed Z endstop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1697,7 +1697,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR true
+#define INVERT_Z_DIR false
 
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
@@ -1739,7 +1739,7 @@
 // :[-1,1]
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
-#define Z_HOME_DIR -1
+#define Z_HOME_DIR 1
 //#define I_HOME_DIR -1
 //#define J_HOME_DIR -1
 //#define K_HOME_DIR -1
