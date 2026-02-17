de4c5111d5634cb727157b7af2f851762cb45cd9
samluethy <sluethy@instapathbio.com>
2026-02-17 11:07:53 -0500

    Sam's pot values

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4155,10 +4155,15 @@ extern bool JOYSTICK_ENABLE;  //Use M119 with JOYSTICK_DEBUG to find reasonable 
   // #define JOYSTICK_DEBUG
 
   // Tom 02/13/2026
-    #define JOY_X_LIMITS { 0, 33200-5000, 33200+5000, 65535 } // min, deadzone start, deadzone end, max
-  #define JOY_Y_LIMITS { 0, 36000-5000, 36000+5000, 65535 }
-  #define JOY_Z_LIMITS { 0, 32000-4000, 32000+4000, 65535 }
-  #define JOYSTICK_DEBUG
+    //#define JOY_X_LIMITS { 0, 33200-5000, 33200+5000, 65535 } // min, deadzone start, deadzone end, max
+  //#define JOY_Y_LIMITS { 0, 36000-5000, 36000+5000, 65535 }
+  //#define JOY_Z_LIMITS { 0, 32000-4000, 32000+4000, 65535 }
+  //#define JOYSTICK_DEBUG
+
+          //Sam's Nova, November 2025
+  #define JOY_X_LIMITS { 0, 34700-5000, 34700+5000, 65535 } // min, deadzone start, deadzone end, max
+  #define JOY_Y_LIMITS { 0, 33400-5000, 33400+5000, 65535 }
+  #define JOY_Z_LIMITS { 0, 32700-4000, 32700+4000, 65535 }
 
 #endif
 
